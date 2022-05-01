3348e0d9807b637ac1a063c6e0f9920ec8d7c871
GabrielHaveroth <haverothgabriel@gmail.com>
2022-05-01 17:16:01 -0300

    Modularizated logic

--- prepare_data_oxford.py
+++ prepare_data_oxford.py
@@ -1,11 +1,11 @@
-from operator import gt
 from numpy import dtype
 import radar.build.radar_processor
 from os import listdir
 from os.path import isfile, join
 import numpy as np
 import os
-
+import multiprocessing  as mp
+from functools import partial
 
 def get_groundtruth_odometry(gt_path, t1, t2):
     gt_data = []
@@ -20,29 +20,114 @@ def get_groundtruth_odometry(gt_path, t1, t2):
     return gt_data
 
 
-root_path = '/home/gabs/Downloads/oxford_radar_robotcar_dataset_sample_medium/'
-seqs = ['2019-01-10-14-36-48-radar-oxford-10k-partial']
+root_path = '/mnt/ubuntu_2004/home/gabs/datasets/'
+seqs = ['2019-01-10-11-46-21-radar-oxford-10k']
 # randomize ordering of image pairs
 
-for seq in seqs:
+seqs = ['2019-01-10-11-46-21-radar-oxford-10k',
+        '2019-01-10-12-32-52-radar-oxford-10k',
+        '2019-01-10-14-02-34-radar-oxford-10k',
+        '2019-01-10-14-36-48-radar-oxford-10k-partial',
+        '2019-01-10-14-50-05-radar-oxford-10k',
+        '2019-01-10-15-19-41-radar-oxford-10k',
+        '2019-01-11-12-26-55-radar-oxford-10k',
+        '2019-01-11-13-24-51-radar-oxford-10k',
+        '2019-01-11-14-02-26-radar-oxford-10k',
+        '2019-01-11-14-37-14-radar-oxford-10k',
+        '2019-01-14-12-05-52-radar-oxford-10k',
+        '2019-01-14-12-41-28-radar-oxford-10k',
+        '2019-01-14-13-38-21-radar-oxford-10k',
+        '2019-01-14-14-15-12-radar-oxford-10k',
+        '2019-01-14-14-48-55-radar-oxford-10k',
+        '2019-01-15-12-01-32-radar-oxford-10k',
+        '2019-01-15-12-52-32-radar-oxford-10k-partial',
+        '2019-01-15-13-06-37-radar-oxford-10k',
+        '2019-01-15-13-53-14-radar-oxford-10k',
+        '2019-01-15-14-24-38-radar-oxford-10k',
+        '2019-01-16-11-53-11-radar-oxford-10k',
+        '2019-01-16-13-09-37-radar-oxford-10k',
+        '2019-01-16-13-42-28-radar-oxford-10k',
+        '2019-01-16-14-15-33-radar-oxford-10k',
+        '2019-01-17-11-46-31-radar-oxford-10k',
+        '2019-01-17-12-48-25-radar-oxford-10k',
+        '2019-01-17-13-26-39-radar-oxford-10k',
+        '2019-01-17-14-03-00-radar-oxford-10k',
+        '2019-01-18-12-42-34-radar-oxford-10k',
+        '2019-01-18-14-14-42-radar-oxford-10k',
+        '2019-01-18-14-46-59-radar-oxford-10k',
+        '2019-01-18-15-20-12-radar-oxford-10k']
+
+def create_pairs(seq):
+    pairs_data = []
     radar_path = root_path + seq + '/radar'
     gt_path = root_path + seq + '/gt/radar_odometry.csv'
-    radar_files = [f for f in listdir(
-        radar_path) if isfile(join(radar_path, f))]
-    radar_files.sort(key=lambda f: int(f.split('.')[0]))
-    correspondence_path = 'correspondences/' + seq
+    correspondence_path = root_path + '/correspondences/' + seq
     try:
         os.mkdir(correspondence_path)
     except OSError as error:
         print(error)
+    radar_files = [f for f in listdir(
+        radar_path) if isfile(join(radar_path, f))]
+    radar_files.sort(key=lambda f: int(f.split('.')[0]))
     for i in range(1, len(radar_files) - 1):
         t1 = int(radar_files[i].split('.')[0])
         t2 = int(radar_files[i + 1].split('.')[0])
-        gt_data = get_groundtruth_odometry(gt_path, t1, t2)
-        if gt_data is not None:
-            correspondeces = radar.build.radar_processor.get_radar_correspondeces(radar_path, radar_files[i - 1], radar_files[i], 1)
-            correspondeces.append(np.array(gt_data))
-        else:
-            print("pair don't have GT")
+        # gt_data = get_groundtruth_odometry(gt_path, t1, t2)
+        pairs_data.append({'radar_imgs': (radar_files[i - 1], radar_files[i]),
+                           'times': (t1, t2),
+                           'radar_path': radar_path,
+                           'gt_path': gt_path,
+                           'correspondence_path': correspondence_path})
+
+    return pairs_data
+
+
+def save_correspondences_pair_gt(pair):
+    t1, t2 = pair['times']
+    gt_path = pair['gt_path']
+    correspondence_path = pair['correspondence_path']
+    gt_data = get_groundtruth_odometry(gt_path, t1, t2)
+
+    if gt_data is not None:
+        correspondences = radar.build.radar_processor.get_radar_correspondences(pair['radar_path'], pair['radar_imgs'][0], pair['radar_imgs'][1], 1)
+        correspondences.append(np.array(gt_data))
         np.save(correspondence_path + '/' + 'pair_%d_%d.npy' %
-                (t1, t2), correspondeces)
+                (t1, t2), correspondences)
+
+    else:
+        print("pair don't have GT")
+
+for seq in seqs:
+    pairs = create_pairs(seq)
+    # with mp.Pool(mp.cpu_count()) as pool:
+    #     global mtx
+    #     mtx = mp.Lock()
+    #     func = partial(save_correspondences_pair_gt, mtx)
+    #     pool.map(func, pairs)
+    for pair in pairs:
+        save_correspondences_pair_gt(pair)
+
+
+print(pairs)
+# for seq in seqs:
+#     radar_path = root_path + seq + '/radar'
+#     gt_path = root_path + seq + '/gt/radar_odometry.csv'
+#     radar_files = [f for f in listdir(
+#         radar_path) if isfile(join(radar_path, f))]
+#     radar_files.sort(key=lambda f: int(f.split('.')[0]))
+#     correspondence_path = '/mnt/ubuntu_2004/home/gabs/datasets/correspondences/' + seq
+#     try:
+#         os.mkdir(correspondence_path)
+#     except OSError as error:
+#         print(error)
+#     for i in range(1, len(radar_files) - 1):
+#         t1 = int(radar_files[i].split('.')[0])
+#         t2 = int(radar_files[i + 1].split('.')[0])
+#         gt_data = get_groundtruth_odometry(gt_path, t1, t2)
+#         if gt_data is not None:
+#             correspondeces = radar.build.radar_processor.get_radar_correspondeces(radar_path, radar_files[i - 1], radar_files[i], 1)
+#             correspondeces.append(np.array(gt_data))
+#         else:
+#             print("pair don't have GT")
+#         np.save(correspondence_path + '/' + 'pair_%d_%d.npy' %
+#                 (t1, t2), correspondeces)
